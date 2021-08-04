ee2b1d407d6b6b6845944e1e526c4fdf12615ee9
desarrollowebguatemala <88248548+desarrollowebguatemala@users.noreply.github.com>
2021-08-03 19:24:06 -0600

    Add files via upload

--- src/type.js
+++ src/type.js
@@ -1,6 +1,6 @@
 export default function texto(){
     $('#ejemplo1').typeIt({
-        strings: ["ReactJs", "Angular", "JavaScript","UX y IU", "Single Page Aplication", "NodeJs", "Firebase", "MongoDB"],
+        strings: ["ReactJs", "Angular", "JavaScript","UX y IU", "SPA", "NodeJs", "Firebase", "MongoDB"],
         speed: 50,
         breakLines: false,
         loop: true
