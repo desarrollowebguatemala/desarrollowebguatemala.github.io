d3927dc9946c155cc6a56610a51ccfd3d5cf0d9a
desarrollowebguatemala <88248548+desarrollowebguatemala@users.noreply.github.com>
2021-09-04 17:49:11 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import texto from "/src/type.js";
 import menu from "/src/menu.js"
 window.addEventListener("DOMContentLoaded", texto())
-document.getElementById("ham").addEventListener("click", menu)
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
